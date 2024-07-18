77458276f26e583b1888b89933cb2fd3458066b2
Burak KECECI <bburakkececi@gmail.com>
2024-07-18 21:35:50 +0300

    expertise entities are added.

---
+++ src/borusanNext/Domain/Entities/ExpertizeResult.cs
@@ -0,0 +1,15 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class ExpertizeResult : Entity<Guid>
+{
+    public int CarDamageInformationRecord { get; set; }
+    public DateTime InquiryDate { get; set; }
+    public List<ChassisPart> ChassisParts { get; set; }
+    public List<BodyShellPart> BodyParts { get; set; }
+}
