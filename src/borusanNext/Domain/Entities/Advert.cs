5889a105e6fd5685be857c6b4aa3555bd6e9e44b
alacin <alacin@BOTO27161>
2024-07-18 21:08:39 +0300

    update comment line

--- src/borusanNext/Domain/Entities/Advert.cs
+++ src/borusanNext/Domain/Entities/Advert.cs
@@ -14,6 +14,6 @@ public class Advert : Entity<Guid>
     public Guid CarId { get; set; }
     public Guid SellerId { get; set; }
     public virtual ICollection<Seller> Sellers { get; set; }
-    //public virtual Car Car{get;set;}
+    public virtual Car Car{get;set;}
 
 }
