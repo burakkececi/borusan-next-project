9c459b26796455c2149e0e8df035ee55d44acb94
Burak KECECI <bburakkececi@gmail.com>
2024-07-22 07:56:43 +0300

    campaign entity is added

---
+++ src/borusanNext/Domain/Entities/Campaign.cs
@@ -0,0 +1,14 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Campaign : Entity<Guid>
+{
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Banner { get; set; }  
+}
