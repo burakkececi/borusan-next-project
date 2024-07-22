e91d1e1fc1fddf1be8b6d0f8526f156d6d0ea4f6
Burak KECECI <bburakkececi@gmail.com>
2024-07-22 07:57:03 +0300

    appointment entity created and its relationships are defined.

---
+++ src/borusanNext/Domain/Entities/Appointment.cs
@@ -0,0 +1,18 @@
+﻿using NArchitecture.Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities;
+public class Appointment : Entity<Guid>
+{
+    public DateTime Date { get; set; }
+    public DateTime Time { get; set; }
+    public Guid CarId { get; set; }
+    public Guid CustomerId { get; set; }
+
+    public virtual Car Car { get; set; }
+    public virtual Customer Customer { get; set; }
+}
