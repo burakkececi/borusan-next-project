394721d7d974e4ae8ce5718a1d53dea164daff38
Burak KECECI <bburakkececi@gmail.com>
2024-07-26 12:45:18 +0300

    brand image service added

--- src/borusanNext/Application/Features/Brands/Commands/Update/UpdateBrandCommand.cs
+++ src/borusanNext/Application/Features/Brands/Commands/Update/UpdateBrandCommand.cs
@@ -6,6 +6,8 @@ using Domain.Entities;
 using NArchitecture.Core.Application.Pipelines.Authorization;
 using MediatR;
 using static Application.Features.Brands.Constants.BrandsOperationClaims;
+using Microsoft.AspNetCore.Http;
+using Application.Services.ImageService;
 
 namespace Application.Features.Brands.Commands.Update;
 
@@ -13,7 +15,7 @@ public class UpdateBrandCommand : IRequest<UpdatedBrandResponse>, ISecuredReques
 {
     public Guid Id { get; set; }
     public required string Name { get; set; }
-    public required string Logo { get; set; }
+    public required IFormFile Logo { get; set; }
 
     public string[] Roles => [Admin, Write, BrandsOperationClaims.Update];
 
@@ -22,20 +24,24 @@ public class UpdateBrandCommand : IRequest<UpdatedBrandResponse>, ISecuredReques
         private readonly IMapper _mapper;
         private readonly IBrandRepository _brandRepository;
         private readonly BrandBusinessRules _brandBusinessRules;
+        private readonly ImageServiceBase _imageServiceBase;
 
         public UpdateBrandCommandHandler(IMapper mapper, IBrandRepository brandRepository,
-                                         BrandBusinessRules brandBusinessRules)
+                                         BrandBusinessRules brandBusinessRules, ImageServiceBase imageServiceBase)
         {
             _mapper = mapper;
             _brandRepository = brandRepository;
             _brandBusinessRules = brandBusinessRules;
+            _imageServiceBase = imageServiceBase;
         }
 
         public async Task<UpdatedBrandResponse> Handle(UpdateBrandCommand request, CancellationToken cancellationToken)
         {
             Brand? brand = await _brandRepository.GetAsync(predicate: b => b.Id == request.Id, cancellationToken: cancellationToken);
             await _brandBusinessRules.BrandShouldExistWhenSelected(brand);
-            brand = _mapper.Map(request, brand);
+
+            brand.Logo = await _imageServiceBase.UpdateAsync(request.Logo, brand.Logo);
+            brand.Name = request.Name;
 
             await _brandRepository.UpdateAsync(brand!);
 
