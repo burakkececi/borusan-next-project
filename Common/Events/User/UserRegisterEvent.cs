aefd628ee6f5fb1c4a10b302c06d63aa0cd56cac
Safa Can Pehlivan <88760711+Sefa4177@users.noreply.github.com>
2024-08-05 16:57:31 +0300

    Rabbit MQ İmplementation

---
+++ Common/Events/User/UserRegisterEvent.cs
@@ -0,0 +1,14 @@
+﻿using MailKit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Events.User;
+public class UserRegisterEvent
+{
+    
+
+
+}
---
+++ Common/RabbitMQ/RabbitMQConstants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.RabbitMQ;
+public class RabbitMQConstants
+{
+    public const string RabbitMQHost = "localhost";
+    public const string DefaultExchangeType = "direct";
+
+    public const string UserExchangeName = "UserExchange";
+    public const string UserRegisterQueueName = "UserRegisterQueue";
+
+}
