5b3698ad69d5cfe826be7103f9798a17b3ee9266
Safa Can Pehlivan <88760711+Sefa4177@users.noreply.github.com>
2024-08-05 17:09:58 +0300

    RabbitMQ implementation added in common library

---
+++ Common/Events/User/UserRegisterEvent.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Events.User;
+public class UserRegisterEvent
+{
+    // buraya user kayıt olunca olacak olan olaylar gelicek.
+}
