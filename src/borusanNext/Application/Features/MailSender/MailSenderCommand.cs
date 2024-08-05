96780cbf6ba9b290f8614b8ea94bf76edf534ca6
Safa Can Pehlivan <88760711+Sefa4177@users.noreply.github.com>
2024-08-05 13:28:39 +0300

    Mail Sender added with narchtemp

---
+++ src/borusanNext/Application/Features/MailSender/MailSenderCommand.cs
@@ -0,0 +1,51 @@
+﻿using MailKit;
+using MediatR;
+using MimeKit;
+using NArchitecture.Core.Mailing;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using IMailService = NArchitecture.Core.Mailing.IMailService;
+
+namespace Application.Features.MailSender
+{
+    public class MailSenderCommand : IRequest<bool>
+    {
+        public List<string> ToEmailList { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+
+    public class MailSenderCommandHandler : IRequestHandler<MailSenderCommand, bool>
+    {
+        private readonly IMailService _mailService;
+
+        public MailSenderCommandHandler(IMailService mailService)
+        {
+            _mailService = mailService;
+        }
+
+        public async Task<bool> Handle(MailSenderCommand request, CancellationToken cancellationToken)
+        {
+            var toEmailList = new List<MailboxAddress>();
+
+            foreach (var toEmail in request.ToEmailList)
+            {
+                toEmailList.Add(new(name: toEmail, toEmail));
+            }
+
+            _mailService.SendMail(
+                new Mail
+                {
+                    ToList = toEmailList,
+                    Subject = request.Subject,
+                    HtmlBody = request.Body
+                }
+            );
+
+            return true;
+        }
+    }
+}
---
+++ src/borusanNext/WebAPI/Controllers/MailSenderController.cs
@@ -0,0 +1,30 @@
+﻿
+using Application.Features.MailSender;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class MailSenderController : ControllerBase
+{
+    private readonly IMediator _mediator;
+
+    public MailSenderController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendMail([FromBody] MailSenderCommand command)
+    {
+        var result = await _mediator.Send(command);
+        if (result)
+        {
+            return Ok("Mail başarılı bir şekilde gönderildi");
+        }
+
+        return StatusCode(500, "Mail gönderme işlemi başarısız oldu.");
+    }
+}
